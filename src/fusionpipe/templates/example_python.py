d7296c21a304ffbe6f273bf1c3a988b42bd09b07
Francesco Carpanese <francesco.carpanese@epfl.ch>
2025-08-10 19:21:41 +0200

    improved user api

--- src/fusionpipe/templates/example_python.py
+++ src/fusionpipe/templates/example_python.py
@@ -1,20 +1,47 @@
 import os
 import sys
 
-from fp_user_utils.user_api import get_current_node_id, get_info_parents, get_current_node_folder_path, get_current_node_folder_path_data
+from fp_user_utils.user_api import (
+    get_current_node_id,
+    get_info_parents,
+    get_current_node_folder_path,
+    get_current_node_folder_path_data,
+    get_parent_info_from_edge_id,
+    get_parent_folder_path_from_edge_id
+)
 
 def print_node_parents():
+    """
+    Example of using api utils to retrieve info of parent node of the current node where this script is run.
+    """
     print("Template fusionpipe")
-    # Put here the id of the node, or fetch from the name of the folder
+    # Get the id of the current node here this script is being run
     node_id = get_current_node_id()
-    # Get the parent folder where you can read the outputs of the parent nodes
+
+    # Get info of all node parents. Read the documentation get_info_parents.
     info_parents = get_info_parents(node_id=node_id)
-    print(f"Parent folders for node {node_id}:\n")
-    for parent in info_parents:
-        print(parent)
-        print('\n')
+    print(f"Retrieved information of the node parents: {info_parents}")
+
+    # In order to retrive information from a given parent of the current node it is recommend to refer to the parent based on the edge_id
+    # Get information of a parent node from edge_id. The resulting dictionary will contain:
+    # {
+    #     'node_id': <parent_node_id>,
+    #     'node_tag': <parent_node_tag>,
+    #     'folder_path': <parent_folder_path>,
+    #     'edge_id': <edge_id>
+    # }
+    edge_id = '01' # Put here the edge_id of interest
+    parent_info = get_parent_info_from_edge_id(edge_id)
+    print(f"Retrieved information of the parent node (edge_id: {edge_id}): {parent_info}")
+
+    # You can get directly the folder path of the parent node from the edge_id
+    edge_id = '01'
+    parent_folder = get_parent_folder_path_from_edge_id(edge_id)
+    print(f"Retrieved folder path of the parent node (edge_id: {edge_id}): {parent_folder}")
+ 
 
 def save_dummy_output():
+
     data_folder_path = get_current_node_folder_path()
     output_path = os.path.join(get_current_node_folder_path_data(), 'dummy.txt') # Get the data folder path for the current node
     with open(output_path, "w") as f:
@@ -23,13 +50,14 @@ def save_dummy_output():
 
 def run_ray_example():
     """
-    Run a Ray example to demonstrate distributed processing on a dataset.
-    This function initializes Ray and applies a remote operation to each item in a dummy dataset in parallel.
+    Run an example using Ray cluster for parallelisation of the workload. See https://docs.ray.io/en/latest/cluster/getting-started.html
+    This function initializes Ray and applies a "remote" operation to each item of dataset in parallel.
 
     Explanation:
+    - "remote" is a decorator that allows a function to be executed in a separate Ray worker.
     - A remote function `process_item` is defined, which simulates processing a dataset item.
     - The dummy dataset is a list of dictionaries.
-    - Each item is processed in parallel using Ray.
+    - Each item is processed in parallel.
     - The results are collected and printed.
     """
 
--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -103,7 +103,7 @@ def test_get_info_parents(pg_test_db, monkeypatch, tmp_path):
     cur = db_utils.init_db(pg_test_db)
     cur.execute('INSERT INTO nodes (node_id, folder_path, node_tag) VALUES (%s, %s, %s)', (parent_id, parent_folder, parent_tag))
     cur.execute('INSERT INTO nodes (node_id, folder_path) VALUES (%s, %s)', (child_id, child_folder))
-    cur.execute('INSERT INTO node_relation (parent_id, child_id) VALUES (%s, %s)', (parent_id, child_id))
+    cur.execute('INSERT INTO node_relation (parent_id, child_id, edge_id) VALUES (%s, %s, %s)', (parent_id, child_id, '01'))
     pg_test_db.commit()
 
     monkeypatch.setattr(user_api, 'connect_to_db', lambda db_url=None: pg_test_db)
@@ -114,7 +114,119 @@ def test_get_info_parents(pg_test_db, monkeypatch, tmp_path):
     assert result[0]['node_id'] == parent_id
     assert result[0]['node_tag'] == parent_tag
     assert result[0]['folder_path'] == parent_folder
+    assert result[0]['edge_id'] == '01'
+
+    cur.close()
+
+def test_get_parent_info_from_edge_id_found(pg_test_db, monkeypatch, tmp_path):
+    # Setup: create parent and child nodes and their relation
+    parent_id = 'n_parent_2'
+    parent_tag = 'parent_tag_2'
+    parent_folder = str(tmp_path / 'parent_folder_2')
+    os.makedirs(parent_folder, exist_ok=True)
+    child_id = 'n_child_2'
+    child_folder = str(tmp_path / 'child_folder_2')
+    os.makedirs(child_folder, exist_ok=True)
+    edge_id = 'edge_42'
+
+    cur = db_utils.init_db(pg_test_db)
+    cur.execute('INSERT INTO nodes (node_id, folder_path, node_tag) VALUES (%s, %s, %s)', (parent_id, parent_folder, parent_tag))
+    cur.execute('INSERT INTO nodes (node_id, folder_path) VALUES (%s, %s)', (child_id, child_folder))
+    cur.execute('INSERT INTO node_relation (parent_id, child_id, edge_id) VALUES (%s, %s, %s)', (parent_id, child_id, edge_id))
+    pg_test_db.commit()
+
+    monkeypatch.setattr(user_api, 'connect_to_db', lambda db_url=None: pg_test_db)
+
+    # Create .node_id file in the child folder
+    node_id_file = os.path.join(child_folder, '.node_id')
+    with open(node_id_file, 'w') as f:
+        f.write(child_id)
+    monkeypatch.chdir(child_folder)
+
+    result = user_api.get_parent_info_from_edge_id(edge_id)
+    assert isinstance(result, dict)
+    assert result['node_id'] == parent_id
+    assert result['node_tag'] == parent_tag
+    assert result['folder_path'] == parent_folder
+    assert result['edge_id'] == edge_id
 
     cur.close()
 
 
+def test_get_parent_info_from_edge_id_not_found(pg_test_db, monkeypatch, tmp_path):
+    # Setup: create child node only, no parent relation
+    child_id = 'n_child_3'
+    child_folder = str(tmp_path / 'child_folder_3')
+    os.makedirs(child_folder, exist_ok=True)
+
+    cur = db_utils.init_db(pg_test_db)
+    cur.execute('INSERT INTO nodes (node_id, folder_path) VALUES (%s, %s)', (child_id, child_folder))
+    pg_test_db.commit()
+
+    monkeypatch.setattr(user_api, 'connect_to_db', lambda db_url=None: pg_test_db)
+
+    # Create .node_id file in the child folder
+    node_id_file = os.path.join(child_folder, '.node_id')
+    with open(node_id_file, 'w') as f:
+        f.write(child_id)
+    monkeypatch.chdir(child_folder)
+
+    result = user_api.get_parent_info_from_edge_id('nonexistent_edge')
+    assert result is None
+
+    cur.close()
+
+
+def test_get_parent_folder_path_from_edge_id_found(pg_test_db, monkeypatch, tmp_path):
+    # Setup: create parent and child nodes and their relation
+    parent_id = 'n_parent_4'
+    parent_tag = 'parent_tag_4'
+    parent_folder = str(tmp_path / 'parent_folder_4')
+    os.makedirs(parent_folder, exist_ok=True)
+    child_id = 'n_child_4'
+    child_folder = str(tmp_path / 'child_folder_4')
+    os.makedirs(child_folder, exist_ok=True)
+    edge_id = 'edge_99'
+
+    cur = db_utils.init_db(pg_test_db)
+    cur.execute('INSERT INTO nodes (node_id, folder_path, node_tag) VALUES (%s, %s, %s)', (parent_id, parent_folder, parent_tag))
+    cur.execute('INSERT INTO nodes (node_id, folder_path) VALUES (%s, %s)', (child_id, child_folder))
+    cur.execute('INSERT INTO node_relation (parent_id, child_id, edge_id) VALUES (%s, %s, %s)', (parent_id, child_id, edge_id))
+    pg_test_db.commit()
+
+    monkeypatch.setattr(user_api, 'connect_to_db', lambda db_url=None: pg_test_db)
+
+    # Create .node_id file in the child folder
+    node_id_file = os.path.join(child_folder, '.node_id')
+    with open(node_id_file, 'w') as f:
+        f.write(child_id)
+    monkeypatch.chdir(child_folder)
+
+    result = user_api.get_parent_folder_path_from_edge_id(edge_id)
+    assert result == parent_folder
+
+    cur.close()
+
+
+def test_get_parent_folder_path_from_edge_id_not_found(pg_test_db, monkeypatch, tmp_path):
+    # Setup: create child node only, no parent relation
+    child_id = 'n_child_5'
+    child_folder = str(tmp_path / 'child_folder_5')
+    os.makedirs(child_folder, exist_ok=True)
+
+    cur = db_utils.init_db(pg_test_db)
+    cur.execute('INSERT INTO nodes (node_id, folder_path) VALUES (%s, %s)', (child_id, child_folder))
+    pg_test_db.commit()
+
+    monkeypatch.setattr(user_api, 'connect_to_db', lambda db_url=None: pg_test_db)
+
+    # Create .node_id file in the child folder
+    node_id_file = os.path.join(child_folder, '.node_id')
+    with open(node_id_file, 'w') as f:
+        f.write(child_id)
+    monkeypatch.chdir(child_folder)
+
+    result = user_api.get_parent_folder_path_from_edge_id('missing_edge')
+    assert result is None
+
+    cur.close()
